feb4528e0e44cecc3c3f09f603d95cd9403c6f68
kirkya99 <47646726+kirkya99@users.noreply.github.com>
2023-06-13 20:13:50 +0200

    Grundlage Head-Component

---
+++ Computer Game Wishlist/src/App.vue
@@ -0,0 +1,38 @@
+<script>
+import Head from './components/Head.vue';
+
+export default{
+  components:{
+    Head
+  },
+  methods:{
+    setTab(value){
+      this.tab = value;
+    }
+  },
+  data(){
+    return{
+      tab: 0
+    }
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    <h1>Computer Games Wunschliste</h1>
+    <Head @filterWishList="setTab"></Head>
+  </div>
+</template>
+
+<style scoped>
+  *{
+    font-family: Arial;
+    background-color: steelblue;
+  }
+  h1{
+    text-align: center;
+    
+  }
+</style>
---
+++ Computer Game Wishlist/src/components/Head.vue
@@ -0,0 +1,56 @@
+<script>
+export default{
+    emits: ['filterWishList'],    
+    data() {
+    return {
+        //hover1: false,
+        //hover2: false
+    }
+  },
+  methods:{
+    switchToGamesList(){
+        this.$emit('filterWishList',0);
+    },
+    switchToWishlist(){
+        this.$emit('filterWishList',1);
+    }
+  }
+}
+</script>
+
+<template>
+    <div id="header">
+        <div class="header_bar">
+            <div class="header_item" role="button" id="gameslist" @click="switchToGamesList">
+                <h2>Neue Spiele</h2>
+            </div>
+            <div class="header_item" role="button" id="wishlist" @click="switchToWishlist">
+                <h2>Wunschliste</h2>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    *{
+        font-family: Arial;
+    }
+    #header{
+        width: 100%;
+        background-color: lightblue;
+    }
+    .header_bar{
+        display: flex;
+        background-color: dodgerblue;
+    }
+    .header_item{
+        width: 50%;
+        text-align: center;
+    }
+    #gameslist:hover{
+        background-color: lightblue;
+    }
+    #wishlist:hover{
+        background-color: lightblue;
+    }
+</style>
