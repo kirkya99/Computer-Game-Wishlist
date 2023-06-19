2bee1353d938101eb39dd4e2e37d1af5b1bc7bf8
kirkya99 <47646726+kirkya99@users.noreply.github.com>
2023-06-19 16:35:32 +0200

    API added

--- Computer Game Wishlist/src/components/Body.vue
+++ Computer Game Wishlist/src/components/Body.vue
@@ -1,9 +1,29 @@
 <script>
+import axios from 'axios'
+
 export default{
-    props: ['selectedTab']
+    created(){
+        axios.get('https://cors-anywhere.herokuapp.com/https://www.mmobomb.com/api1/games')
+            .then(response => {
+                console.log(response.data)
+            })
+            .catch(console.error)
+    },
+    props: ['selectedTab'],
+    components:{
+        axios
+    },
+    data(){
+        return{
+            gamesArray: []
+        }
+    },   
 }
+
+    
 </script>
 
 <template>
+
     {{ selectedTab }}
 </template>
